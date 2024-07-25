fdb6c7cd59d60fa8dc051d90e02fbb1a1333c7b8
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-07-25 13:27:58 +0200

    refactor: Update PlaceholdersForm to improve UI and add selected placeholder highlighting

--- src/components/PlaceholdersForm.tsx
+++ src/components/PlaceholdersForm.tsx
@@ -1,17 +1,19 @@
 import { Form, InputGroup } from "react-bootstrap";
 import { useContext } from "react";
-import { PlaceholdersContext } from "../App";
+import { PlaceholdersContext, SelectedPlaceholderContext } from "../App";
 import { useFormContext } from "react-hook-form";
 
 const PlaceholdersForm = () => {
   const { register} = useFormContext();
   const { fields } = useContext(PlaceholdersContext) || {};
+  const { selected } = useContext(SelectedPlaceholderContext) || {};
 
   return (
-    <div className="d-flex flex-column gap-3">
+    <div className="d-flex flex-column gap-3 w-100">
       <h6>Placeholders</h6>
       {fields?.map((field, index) => (
-        <InputGroup key={field.id} style={{ width: "fit-content" }}>
+        <div className="d-flex flex-column gap-1">
+        <InputGroup key={field.id} className={selected === field.uuid ? "border border-2 border-primary rounded" : ""}>
           <InputGroup.Text><div
             style={{
               width: "32px",
@@ -21,10 +23,13 @@ const PlaceholdersForm = () => {
               border: `2px solid ${field.stroke}`,
             }}
           ></div></InputGroup.Text>          
-          <Form.Control {...register(`items.${index}.name`)} />
-          <InputGroup.Text>x: {field.x}</InputGroup.Text>
-          <InputGroup.Text>y: {field.y}</InputGroup.Text>
+          <Form.Control {...register(`items.${index}.name`)} className="w-auto" />
         </InputGroup>
+        <small>x: {field.x}</small>
+        <small>y: {field.y}</small>
+        <small>width: {field.width}</small>
+        <small>height: {field.height}</small>
+        </div>
       ))}
     </div>
   );
