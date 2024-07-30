7e3cebfd1c8761c8bed2f23dc74200a300dd837b
Diego Martignoni <diego.martignoni@lemonpie.it>
2024-07-30 10:58:02 +0200

    Update PlaceholdersForm to improve UI and add selected placeholder highlighting

--- src/components/PlaceholdersForm.tsx
+++ src/components/PlaceholdersForm.tsx
@@ -4,7 +4,7 @@ import { PlaceholdersContext, SelectedPlaceholderContext } from "../App";
 import { useFormContext } from "react-hook-form";
 
 const PlaceholdersForm = () => {
-  const { register} = useFormContext();
+  const { register } = useFormContext();
   const { fields } = useContext(PlaceholdersContext) || {};
   const { selected } = useContext(SelectedPlaceholderContext) || {};
 
@@ -12,23 +12,34 @@ const PlaceholdersForm = () => {
     <div className="d-flex flex-column gap-3 w-100">
       <h6>Placeholders</h6>
       {fields?.map((field, index) => (
-        <div className="d-flex flex-column gap-1">
-        <InputGroup key={field.id} className={selected === field.uuid ? "border border-2 border-primary rounded" : ""}>
-          <InputGroup.Text><div
-            style={{
-              width: "32px",
-              height: "32px",
-              borderRadius: "50%",
-              backgroundColor: field.fill,
-              border: `2px solid ${field.stroke}`,
-            }}
-          ></div></InputGroup.Text>          
-          <Form.Control {...register(`items.${index}.name`)} className="w-auto" />
-        </InputGroup>
-        <small>x: {field.x}</small>
-        <small>y: {field.y}</small>
-        <small>width: {field.width}</small>
-        <small>height: {field.height}</small>
+        <div
+          className={`card p-4 d-flex flex-column gap-2 ${
+            selected === field.uuid
+              ? "border border-3 border-primary rounded"
+              : ""
+          }`}
+        >
+          <InputGroup key={field.id}>
+            <InputGroup.Text>
+              <div
+                style={{
+                  width: "32px",
+                  height: "32px",
+                  borderRadius: "50%",
+                  backgroundColor: field.fill,
+                  border: `2px solid ${field.stroke}`,
+                }}
+              ></div>
+            </InputGroup.Text>
+            <Form.Control
+              {...register(`items.${index}.name`)}
+              className="w-auto"
+            />
+          </InputGroup>
+          <small>x: {field.x}</small>
+          <small>y: {field.y}</small>
+          <small>width: {field.width}</small>
+          <small>height: {field.height}</small>
         </div>
       ))}
     </div>
